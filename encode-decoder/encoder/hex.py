0aca1c8213b5de2658daf709d4d4a6c7ddbdb240
Samuel Rajanala <24me1a4643@rcee.ac.in>
2026-04-26 15:33:36 +0530

    Update hex.py

--- encode-decoder/encoder/hex.py
+++ encode-decoder/encoder/hex.py
@@ -2,4 +2,7 @@ def encode(data: str) -> str:
     return data.encode().hex()
 
 def decode(data: str) -> str:
-    return bytes.fromhex(data).decode(errors="ignore")
+    try:
+        return bytes.fromhex(data).decode(errors="ignore")
+    except ValueError:
+        return "❌ Invalid hex input"
